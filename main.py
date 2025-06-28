7faf4ac5ed18f37b8e8d67a71ac4dad6d5f554ad
서하성 <shsboss274@gmail.com>
2025-06-28 18:24:08 +0900

    🔧 auth 라우터 추가 및 관리자 버튼 표시 오류 수정

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from database import Base, engine, SessionLocal, User
 from routers import auth, admin
+from routers import admin_users
+
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="서하성")
@@ -16,7 +18,7 @@ Base.metadata.create_all(bind=engine)
 # 라우터 등록
 app.include_router(auth.router)         # 로그인 등
 app.include_router(admin.router)        # 관리자 관련
-
+app.include_router(admin_users.router, prefix="/admin")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
