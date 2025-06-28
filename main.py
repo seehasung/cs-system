ef5e6a2b6b620956eb8639d20c4899050157e974
서하성 <shsboss274@gmail.com>
2025-06-28 17:04:24 +0900

    🔧 auth 라우터 추가 및 관리자 버튼 표시 오류 수정

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Base.metadata.create_all(bind=engine)
 
 # ✅ auth 라우터 등록
 app.include_router(auth.router)
-app.include_router(admin.router)
+app.include_router(admin.router, prefix="/admin")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
